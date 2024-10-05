b2dea75a7984684e0f88d30e542b5418796ffb09
“richardcrn3rd” <richardcrn3rd@gmail.com>
2024-10-05 16:32:29 -0400

    Add Homepage Route

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+require ('dotenv').config();
+
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-    res.send('Hello world!')
+// Color Page
+app.get('/:color', function (req, res) {
+    let myColor = req.params.color
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1 style= "color: ${myColor};">${myColor} Page</h1>
+        </body>
+    `)
 })
 
-app.listen(3000)
+
+app.listen(process.env.PORT)
