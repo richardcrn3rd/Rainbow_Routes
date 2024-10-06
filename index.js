0ef5f95818d5340daa3c8437259e93645e5f5bab
“richardcrn3rd” <richardcrn3rd@gmail.com>
2024-10-06 14:41:12 -0400

    Added 404 Not Here

--- index.js
+++ index.js
@@ -4,17 +4,27 @@ const express = require('express')
 const app = express()
 
 // Color Page
-app.get('/:color', function (req, res) {
+app.get('/:places', function (req, res) {
     let myColor = req.params.color
     res.send(`
         <body style="margin: 0;">
             <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
                 <h2 style="text-align: center;">NAV BAR</h2>
             </div>
-            <h1 style= "color: ${myColor};">${myColor} Page</h1>
+            <h1>404 NOT here!!!!</h1>
         </body>
     `)
 })
 
+app.get('*', function (req, res) {
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
+                <h2 style="text-align: center;">NAV BAR</h2>
+                <h1>404 NOT FOUND</h1>
+            </div>
+        </body>
+    `)
+})
 
 app.listen(process.env.PORT)
